04dfc295862677a213f311f54f15762c9f8f04a6
Maria Sidorova <mariasidorova@MacBook-Air-Maria.local>
2026-01-27 14:58:39 +0800

    refactoring hook useFavorites

--- src/hooks/useFavorites.jsx
+++ src/hooks/useFavorites.jsx
@@ -1,10 +1,42 @@
-import { useContext } from "react";
+import { useEffect, useState } from "react";
 import { FavoritesContext } from "../context/FavoritesContext";
 
 export const useFavorites = () => {
-  const context = useContext(FavoritesContext);
-  if (!context) {
-    throw new Error("useFavorites must be used inside FavoritesProvider");
-  }
-  return context;
+  const [favorites, setFavorites] = useState([]);
+  useEffect(() => {
+    const stored = localStorage.getItem("favorites");
+    if (stored) {
+      setFavorites(JSON.parse(stored));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("favorites", JSON.stringify(favorites));
+  }, [favorites]);
+
+  const addFavorites = (item) => {
+    setFavorites((prev) => [...prev, item]);
+  };
+
+  const removeFavorites = (id) => {
+    setFavorites((prev) => prev.filter((item) => item.id !== id));
+  };
+
+  const isFavorite = (id) => favorites.some((item) => item.id === id);
+
+  const toggleFavorites = (item) => {
+    isFavorite(item.id) ? removeFavorites(item.id) : addFavorites(item);
+  };
+
+  return {
+    addFavorites,
+    removeFavorites,
+    isFavorite,
+    toggleFavorites,
+  };
+  // const context = useContext(FavoritesContext);
+  // if (!context) {
+  //   throw new Error("useFavorites must be used inside FavoritesProvider");
+  // }
+  // return context;
 };
